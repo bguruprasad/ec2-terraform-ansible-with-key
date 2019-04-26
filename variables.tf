4ccdea0cbfec8f9cca0c0c476a33d5ddab07172c
Guruprasad Bhavsar <guruprasad.bhavsar@gmail.com>
2019-04-26 10:09:05 +0100

    Added description to variables

--- variables.tf
+++ variables.tf
@@ -1,5 +1,10 @@
-variable "keypath" {}
-variable "ami" {}
+variable "keypath" {
+	description = "SSH Public key path to be used"
+}
+variable "ami" {
+	description = "AWS AMI to be used for spinning up the instance"
+}
 variable "instance_type" {
 	default = "t2.micro"
+	description = "EC2 Instance type"
 }
